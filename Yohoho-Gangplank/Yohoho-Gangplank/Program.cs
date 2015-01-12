a99c1f6ae2a26a235373bd365b32cb5f24ce2c48
nongnoobjung <kitcharuk_4@hotmail.com>
2015-01-12 16:20:55 +0700

    Update

--- Yohoho-Gangplank/Yohoho-Gangplank/Program.cs
+++ Yohoho-Gangplank/Yohoho-Gangplank/Program.cs
@@ -19,6 +19,7 @@ namespace Yohoho_Gangplank
         public static Spell R;
         public static Obj_AI_Hero Player = ObjectManager.Player;
         public static Orbwalking.Orbwalker Orbwalker;
+        public static SpellSlot igniteSlot;
 
         static void Main(string[] args)
         {
@@ -34,6 +35,7 @@ namespace Yohoho_Gangplank
             W = new Spell(SpellSlot.W);
             E = new Spell(SpellSlot.E);
             R = new Spell(SpellSlot.R, 25000);
+            igniteSlot = Player.GetSpellSlot("SummonerDot");
 
             //Config Menu
             Config = new Menu("Gankplank", "Gankplank", true);
@@ -55,12 +57,20 @@ namespace Yohoho_Gangplank
             //Harass
             Config.AddSubMenu(new Menu("Harass", "Harass"));
             Config.SubMenu("Harass").AddItem(new MenuItem("UseQH", "Use Q ").SetValue(true));
+            Config.SubMenu("Harass").AddItem(new MenuItem("Hmana", "Min. Mana Percent Harass").SetValue(new Slider(50, 100, 0)));
+
+            //Farm 
+            Config.AddSubMenu(new Menu("Farm", "Farm"));
+            Config.SubMenu("Farm").AddItem(new MenuItem("AutoQF", "Auto Q FARM ").SetValue(new KeyBind("Y".ToCharArray()[0],
+        KeyBindType.Toggle)));
+            Config.SubMenu("Farm").AddItem(new MenuItem("Fmana", "Min. Mana Percent AutoQ").SetValue(new Slider(50, 100, 0)));
 
             //Misc Menu
             Config.AddSubMenu(new Menu("Misc", "Misc"));
             Config.SubMenu("Misc").AddItem(new MenuItem("AutoW", "AutoW Remove CC").SetValue(true));
             Config.SubMenu("Misc").AddItem(new MenuItem("UseRKs", "Auto R KS").SetValue(true));
             Config.SubMenu("Misc").AddItem(new MenuItem("UseQKs", "Auto Q KS").SetValue(true));
+            Config.SubMenu("KS").AddItem(new MenuItem("IgKs", "Use Ignite KS").SetValue(true));
 
             //DrawEmenu
             Config.AddSubMenu(new Menu("Draw", "DrawSettings"));
@@ -77,15 +87,18 @@ namespace Yohoho_Gangplank
         {
             if (Player.IsDead) return;
             if (Config.Item("DrawQ").GetValue<bool>() && Q.Level > 0) Utility.DrawCircle(Player.Position, Q.Range, System.Drawing.Color.Red);
-            
+
         }
 
         static void Game_OnGameUpdate(EventArgs args)
         {
             if (Player.IsDead) return;
+
+
             KS();
             autoW();
-            if (Orbwalker.ActiveMode ==  Orbwalking.OrbwalkingMode.Combo)
+
+            if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
             {
                 Combo();
             }
@@ -93,7 +106,30 @@ namespace Yohoho_Gangplank
             {
                 Harass();
             }
-            
+            if (Config.Item("AutoQF").GetValue<KeyBind>().Active)
+            {
+                AutoFarm();
+            }
+
+
+        }
+
+        static void AutoFarm()
+        {
+
+
+            //credit to DedToto
+            var minions = MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.Enemy, MinionOrderTypes.MaxHealth).FirstOrDefault(m => m.Health < ((Player.GetSpellDamage(m, SpellSlot.Q) - 20)) && HealthPrediction.GetHealthPrediction(m, (int)(Player.Distance(m, false) / Q.Speed), (int)(Q.Delay * 1000 + Game.Ping / 2)) > 0);
+
+            var existsMana = ObjectManager.Player.MaxMana / 100 * Config.Item("Fmana").GetValue<Slider>().Value;
+            if (Player.Mana >= existsMana)
+            {
+                if (Q.IsReady() && Player.Distance(minions) < Q.Range)
+                {
+                    //Game.PrintChat("FarmQ");
+                    Q.CastOnUnit(minions);
+                }
+            }
         }
 
         static void Combo()
@@ -104,6 +140,11 @@ namespace Yohoho_Gangplank
             var useR = Config.Item("UseRC").GetValue<bool>();
             var hitR = Config.Item("UseRCombo1").GetValue<Slider>().Value;
 
+
+            if (E.IsReady() && useE)
+            {
+                E.Cast();
+            }
             if (Q.IsReady() && Player.Distance(Target) <= Q.Range && useQ)
             {
                 Q.CastOnUnit(Target);
@@ -113,39 +154,40 @@ namespace Yohoho_Gangplank
             {
                 R.CastIfWillHit(Target, hitR);
             }
-            if (E.IsReady() && useE)
-            {
-                if (Player.Distance(Target) > Q.Range || Target.CountEnemysInRange(1000) > 2)
-                {
-                    E.Cast();
-                }
-            }
+
 
         }
 
         static void Harass()
         {
             var Target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Physical);
-
-            if (Q.IsReady() && Player.Distance(Target) <= Q.Range && Config.Item("UseQH").GetValue<bool>())
+            var existsMana = ObjectManager.Player.MaxMana / 100 * Config.Item("Hmana").GetValue<Slider>().Value;
+            if (Player.Mana >= existsMana)
             {
-                Q.CastOnUnit(Target);
+                if (Q.IsReady() && Player.Distance(Target) <= Q.Range && Config.Item("UseQH").GetValue<bool>())
+                {
+                    Q.CastOnUnit(Target);
+                }
             }
+
+
 
         }
 
-        static void autoW(){
+        static void autoW()
+        {
             if (!Config.Item("AutoW").GetValue<bool>()) return;
 
-            if(Player.HasBuffOfType(BuffType.Taunt) || Player.HasBuffOfType(BuffType.Stun) || Player.HasBuffOfType(BuffType.Snare) ||
+            if (Player.HasBuffOfType(BuffType.Taunt) || Player.HasBuffOfType(BuffType.Stun) || Player.HasBuffOfType(BuffType.Snare) ||
                 Player.HasBuffOfType(BuffType.Polymorph) || Player.HasBuffOfType(BuffType.Blind) || Player.HasBuffOfType(BuffType.Fear) || Player.HasBuffOfType(BuffType.Silence))
             {
                 if (W.IsReady())
                 {
+                    Game.PrintChat("AutoW");
                     W.Cast();
                 }
             }
-            
+
 
         }
 
@@ -153,48 +195,33 @@ namespace Yohoho_Gangplank
         {
             if (!Config.Item("UseRKs").GetValue<bool>() && !Config.Item("UseQKs").GetValue<bool>()) return;
 
-            //Q KS
-           foreach (Obj_AI_Hero target in ObjectManager.Get<Obj_AI_Hero>().Where(x => Player.Distance(x) < Q.Range && x.IsValidTarget() && x.IsEnemy && !x.IsDead))
-           {
-               if (target != null || !Config.Item("UseQKs").GetValue<bool>())
+
+            foreach (var target in ObjectManager.Get<Obj_AI_Hero>())
+            {
+
+                if (Config.Item("UseQKs").GetValue<bool>() && !target.IsDead && Q.IsReady() && !target.IsAlly && Player.Distance(target.Position) < Q.Range && Player.GetSpellDamage(target, SpellSlot.Q) > (target.Health + 20))
                 {
-                    //Q
-                    if (Player.Distance(target.ServerPosition) <= Q.Range &&
-                        (Player.GetSpellDamage(target, SpellSlot.Q)) > target.Health+20) 
-                    {
-                        if (Q.IsReady())
-                        {
-                            Q.CastOnUnit(target);
-                            return;
-                        }
-                    }
-
-
+                    Game.PrintChat("Qks");
+                    Q.CastOnUnit(target);
                 }
+
+
+                if (Config.Item("UseRKs").GetValue<bool>() && R.IsReady() && !target.IsDead && !target.IsAlly && Player.Distance(target.Position) < R.Range && Player.GetSpellDamage(target, SpellSlot.R) > (target.Health))
+                {
+                    Game.PrintChat("Rks");
+                    R.Cast(target);
+                }
+
+                if (igniteSlot != SpellSlot.Unknown && ObjectManager.Player.Spellbook.CanUseSpell(igniteSlot) == SpellState.Ready && ObjectManager.Player.Distance(target.Position) < 600 && Config.Item("IgKs").GetValue<bool>())
+                {
+                    if (ObjectManager.Player.GetSummonerSpellDamage(target, Damage.SummonerSpell.Ignite) > target.Health)
+                    {
+                        ObjectManager.Player.Spellbook.CastSpell(igniteSlot, target);
+                    }
+                }
+
             }
-            //End Q
-            //R KS
-           foreach (Obj_AI_Hero target in ObjectManager.Get<Obj_AI_Hero>().Where(x => Player.Distance(x) < R.Range && x.IsValidTarget() && x.IsEnemy && !x.IsDead))
-           {
-               if (target != null || Config.Item("UseRKs").GetValue<bool>())
-               {
-                   //R
-                   if (Player.Distance(target.ServerPosition) <= R.Range &&
-                       (Player.GetSpellDamage(target, SpellSlot.R)) > target.Health + 100) 
-                   {
-                       if (R.IsReady())
-                       {
-                           R.Cast(target);
-                           return;
-                       }
-                   }
 
-
-               }
-           }
-            //End R
-            
         }
-
     }
 }
